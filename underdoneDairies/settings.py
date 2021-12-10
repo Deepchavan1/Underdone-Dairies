e3c8b41c2d85e01f0b935d2a2bd16b83f0cb0523
Deepchavan1 <dc561268@gmail.com>
2021-12-10 20:51:45 +0530

    deploy

--- underdoneDairies/settings.py
+++ underdoneDairies/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a&yy^ao^(@s8h@uu7azc&n6!a@v&#y$!$tgfqcpv!5+6zzd5yx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,8 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
 ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
--- underdoneDairies/urls.py
+++ underdoneDairies/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "Underdone Dairies Admin"
 
@@ -29,4 +31,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('poem/', include('poem.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
