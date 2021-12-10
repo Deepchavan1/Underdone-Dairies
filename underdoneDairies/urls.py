3996cad176d40c7db8d7fed1e03adab728687604
Deepchavan1 <dc561268@gmail.com>
2021-12-10 21:19:13 +0530

    url

--- underdoneDairies/urls.py
+++ underdoneDairies/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
