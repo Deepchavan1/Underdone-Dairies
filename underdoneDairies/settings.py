e81078c30f463ae4cb28737cb7f7840fbadae717
Deepchavan1 <dc561268@gmail.com>
2021-12-10 21:57:21 +0530

    add

--- underdoneDairies/settings.py
+++ underdoneDairies/settings.py
@@ -136,6 +136,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
 ]
